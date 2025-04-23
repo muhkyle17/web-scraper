8a330d671efe68acbda8c7c915f5f144587c4174
Mikyle Reyes <reyesmikyle17@gmail.com>
2025-04-23 10:26:16 -0700

    Add further logic for getting the url scrape

--- server/utils/scraper.js
+++ server/utils/scraper.js
@@ -12,6 +12,21 @@ export const scrapeListings = async ({ browser, retryCount }) => {
   try {
     const page = await browser.newPage()
     console.log(page, 'page')
+
+    try {
+      await page.goto('https://www.airbnb.com/', { waitUntil: 'load' })
+
+      await page.waitForSelector('[itemprop="itemListElement"]', {
+        timeout: 10000,
+      })
+    } catch (pageError) {
+      if (retryCount < MAX_RETRIES) {
+        console.log(`Retrying... (${retryCount + 1}/${MAX_RETRIES})`)
+        return await scrapeListings(retryCount + 1)
+      }
+    } finally {
+      await page.close
+    }
   } catch (browserError) {
     throw new Error(`Failed to launch browser: ${browserError.message}`)
   }
